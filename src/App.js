8e7199ff28d2196f733c6e5622a00d280dc057ce
Ali Cebe <alicebe86@gmail.com>
2024-11-03 18:10:07 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [userInfo, setUserInfo] = useState({
+    intro: "Welcome to FullStack Development - I",
+    labinfo: "React JS Programming Week09 Lab exercise",
+    studentID:"101205359",
+    studentName:"Ali Cebe",
+    collegeNameCity: "George Brown College, Toronto"
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{userInfo.intro}!</h1>
+        <h2>{userInfo.labinfo}</h2>
+        <h4>{userInfo.studentID}</h4>
+        <h5>{userInfo.studentName}</h5>
+        <h6>{userInfo.collegeNameCity}</h6>
       </header>
     </div>
   );
